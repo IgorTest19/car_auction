1c6d65ebbcdbe756f5975c8cb5c4143e17c08401
IgorTest19 <quattroporte54@gmail.com>
2022-10-11 23:37:19 +0200

    added login and logout functionalities

--- car_auction_service/users/urls.py
+++ car_auction_service/users/urls.py
@@ -1,4 +1,4 @@
-"""car_auction_service URL Configuration
+"""cars URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/4.1/topics/http/urls/
@@ -13,11 +13,14 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
 from django.urls import path, include
+from . import views
+from django.views.generic.base import TemplateView
+
+app_name = 'users'
 
 urlpatterns = [
-    path("admin/", admin.site.urls),
-    path('users/', include('users.urls', namespace='users')),
-    path('', include('cars.urls', namespace='cars')),
+    path("accounts/", include("django.contrib.auth.urls")),
+    # path('', TemplateView.as_view(template_name='home.html'), name='home')
+
 ]

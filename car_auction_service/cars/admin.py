22019a900208ff3864040d7160df22e9b5db93de
IgorTest19 <quattroporte54@gmail.com>
2022-11-06 20:20:15 +0100

    cars admin panel modifications

--- car_auction_service/cars/admin.py
+++ car_auction_service/cars/admin.py
@@ -3,8 +3,8 @@ from .models import Car
 
 # Register your models here.
 
-@admin.register
-class CarAdmin(Car):
-    list_display = ['owner', 'brand', 'model', 'year', 'publish', 'created', 'update', 'photo']
+@admin.register(Car)
+class CarAdmin(admin.ModelAdmin):
+    list_display = ['id', 'owner', 'brand', 'model', 'year', 'publish', 'created', 'update', 'photo']
 
 # admin.site.register(Car)

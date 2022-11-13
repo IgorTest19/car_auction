6fd53a49b930c798d71fab34c311778056a299cf
IgorTest19 <quattroporte54@gmail.com>
2022-11-14 00:15:06 +0100

    filters fix

--- car_auction_service/cars/filters.py
+++ car_auction_service/cars/filters.py
@@ -5,6 +5,6 @@ class CarSearchFilter(django_filters.FilterSet):
     class Meta:
         model = Car
         fields = {
-            'title': ['icontains'],
-            'body': ['icontains'],
+            'brand': ['icontains'],
+            'model': ['icontains'],
         }

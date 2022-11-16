9f1543552e67a527044935d4b149fea38f3d6fb5
IgorTest19 <quattroporte54@gmail.com>
2022-11-16 22:08:48 +0100

    addinf Image form and registering Image in adin panel

--- car_auction_service/cars/admin.py
+++ car_auction_service/cars/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Car
+from .models import Car, Image
 
 # Register your models here.
 
@@ -8,3 +8,7 @@ class CarAdmin(admin.ModelAdmin):
     list_display = ['id', 'owner', 'brand', 'model', 'year', 'publish', 'created', 'update', 'photo']
 
 # admin.site.register(Car)
+
+@admin.register(Image)
+class ImageAdmin(admin.ModelAdmin):
+    list_display = ['car', 'image']

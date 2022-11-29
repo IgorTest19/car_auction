d64ab31a7f687bedf25a972965421c8d0a27e0a0
IgorTest19 <quattroporte54@gmail.com>
2022-11-29 23:45:23 +0100

    adding debug comments

--- car_auction_service/users/views.py
+++ car_auction_service/users/views.py
@@ -5,14 +5,24 @@ from .forms import UserRegistrationForm
 # Create your views here.
 
 def register(request):
+    print('----if request.method == "POST"')
+    print(request.method)
     if request.method == "POST":
+        print("------request.POST")
+        print(request.POST)
         user_form = UserRegistrationForm(request.POST)
+        print('----------user_form')
+        print(user_form)
         if user_form.is_valid():
+            print('-----user_form.is_valid()')
+            print(user_form.is_valid())
             new_user = user_form.save(commit=False)
+            print("-------new_user")
+            print(new_user)
             new_user.set_password(user_form.cleaned_data['password'])
             new_user.save()
-            return render(request, 'account/register_done.html', {'new_user':new_user})
+            return render(request, 'cars/user_dashboard.html', {'new_user':new_user})
     else:
         user_form = UserRegistrationForm()
-    return render(request, 'account/register.html', {'user_form':user_form})
+    return render(request, 'users/register.html', {'user_form':user_form})
     #TODO: add templates register_done and register

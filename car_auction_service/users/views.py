1a98d81510f640be417cad7aa9a05f4047fe3c20
IgorTest19 <quattroporte54@gmail.com>
2022-11-29 23:58:22 +0100

    changed redirect template

--- car_auction_service/users/views.py
+++ car_auction_service/users/views.py
@@ -21,7 +21,7 @@ def register(request):
             print(new_user)
             new_user.set_password(user_form.cleaned_data['password'])
             new_user.save()
-            return render(request, 'cars/user_dashboard.html', {'new_user':new_user})
+            return render(request, 'users/register_done.html', {'new_user':new_user})
     else:
         user_form = UserRegistrationForm()
     return render(request, 'users/register.html', {'user_form':user_form})

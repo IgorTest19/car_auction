04c9b05040f346a47ffac265d1c87043d0af9840
IgorTest19 <quattroporte54@gmail.com>
2022-12-03 13:51:58 +0100

    changes in cars filtering

--- car_auction_service/cars/filters.py
+++ car_auction_service/cars/filters.py
@@ -1,10 +1,15 @@
+from django.forms.widgets import TextInput
 from .models import Car
 import django_filters
 
 class CarSearchFilter(django_filters.FilterSet):
+    brand = django_filters.CharFilter(lookup_expr='icontains', widget=TextInput(attrs={'placeholder': 'Volvo'}))
+    model = django_filters.CharFilter(lookup_expr='icontains', widget=TextInput(attrs={'placeholder': 'V60'}))
+
     class Meta:
         model = Car
-        fields = {
-            'brand': ['icontains'],
-            'model': ['icontains'],
-        }
+        fields = ['brand', 'model']
+        # fields = {
+        #     'brand': ['icontains'],
+        #     'model': ['icontains'],
+        # }

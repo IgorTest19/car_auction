fad133b1a129394054c92700d2a0853f1360f1cc
IgorTest19 <quattroporte54@gmail.com>
2022-12-20 23:39:49 +0100

    adding signals for UserProfile creations and saving

--- car_auction_service/users/models.py
+++ car_auction_service/users/models.py
@@ -1,6 +1,8 @@
 from django.db import models
-from car_auction_service.cars.models import Car
+from cars.models import Car
 from django.contrib.auth.models import User
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 
 # Create your models here.
 
@@ -12,7 +14,16 @@ from django.contrib.auth.models import User
 
 class UserProfile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
-    cars_observed = models.ManyToManyField(Car, related_name='cars_observed', blank=True, null=True)
+    cars_observed = models.ManyToManyField(Car, related_name='cars_observed', blank=True)
 
     def __str__(self):
         return str(self.user.username)
+
+    @receiver(post_save, sender=User)
+    def create_user_profile(sender, instance, created, **kwargs):
+        if created:
+            UserProfile.objects.create(user=instance)
+
+    @receiver(post_save, sender=User)
+    def save_user_profile(sender, instance, **kwargs):
+        instance.profile.save()

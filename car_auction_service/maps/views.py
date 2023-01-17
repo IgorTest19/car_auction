ac064e045ed2bf1f4ae3732d49287f9b0a434399
IgorTest19 <quattroporte54@gmail.com>
2023-01-17 23:36:00 +0100

    adding function in views to map creation

--- car_auction_service/maps/views.py
+++ car_auction_service/maps/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+import folium
 
-# Create your views here.
+
+def maps_index(request):
+    # Create a map object
+    cars_map = folium.Map()
+    cars_map = m._repr_html_()
+    return render(request, 'map.html', {'cars_map': cars_map})

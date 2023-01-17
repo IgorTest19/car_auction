fbcce6e2c4454e7584fec314f33d67cf9f6fb620
IgorTest19 <quattroporte54@gmail.com>
2023-01-17 23:36:29 +0100

    fix in variable name

--- car_auction_service/maps/views.py
+++ car_auction_service/maps/views.py
@@ -5,5 +5,5 @@ import folium
 def maps_index(request):
     # Create a map object
     cars_map = folium.Map()
-    cars_map = m._repr_html_()
+    cars_map = cars_map._repr_html_()
     return render(request, 'map.html', {'cars_map': cars_map})

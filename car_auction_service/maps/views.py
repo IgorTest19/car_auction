731662b8fe122c85bdf513dd24398f77a6015087
IgorTest19 <quattroporte54@gmail.com>
2023-01-19 00:09:16 +0100

    changes in funciton name

--- car_auction_service/maps/views.py
+++ car_auction_service/maps/views.py
@@ -1,8 +1,8 @@
 from django.shortcuts import render
 import folium
 
-
-def maps_index(request):
+# https://www.youtube.com/watch?v=2uFJ43DvhHg&t=810s&ab_channel=KenBroTech
+def maps_main(request):
     # Create a map object
     cars_map = folium.Map()
     cars_map = cars_map._repr_html_()

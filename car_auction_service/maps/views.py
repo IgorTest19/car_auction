c3090d02e486571be5c9054f67def1c8b84b5d87
IgorTest19 <quattroporte54@gmail.com>
2023-01-25 23:30:54 +0100

    Adding marker filed to standalone map app

--- car_auction_service/maps/views.py
+++ car_auction_service/maps/views.py
@@ -9,6 +9,8 @@ STANDALONE MAPS
 def maps_main(request):
     # Creating Map Object
     cars_map = folium.Map(location=[50, 20], zoom_start=6)
+    # Adding map marker
+    folium.Marker([52, 20]).add_to(cars_map)
     # Getting HTML representation of Map Object
     cars_map = cars_map._repr_html_()
     return render(request, 'maps/map_main.html', {'cars_map': cars_map})

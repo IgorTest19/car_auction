0fa975be165aa3d2101f99ecd004837a56ef0e32
IgorTest19 <quattroporte54@gmail.com>
2023-01-25 23:26:48 +0100

    changed initial values

--- car_auction_service/maps/views.py
+++ car_auction_service/maps/views.py
@@ -7,8 +7,8 @@ STANDALONE MAPS
 # https://www.youtube.com/watch?v=2uFJ43DvhHg&t=810s&ab_channel=KenBroTech
 # http://127.0.0.1:8000/maps/maps_main/
 def maps_main(request):
-    # Create a Map Object
-    cars_map = folium.Map(location=[19, -12], zoom_start=1)
-    # Get hmtl representation of Map Object
+    # Creating Map Object
+    cars_map = folium.Map(location=[50, 20], zoom_start=6)
+    # Getting HTML representation of Map Object
     cars_map = cars_map._repr_html_()
     return render(request, 'maps/map_main.html', {'cars_map': cars_map})

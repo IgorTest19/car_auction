af2b59504e607251337d58a24eac577a2fb82f5f
IgorTest19 <quattroporte54@gmail.com>
2023-02-01 22:50:27 +0100

    added location field to forms and filters

--- car_auction_service/cars/filters.py
+++ car_auction_service/cars/filters.py
@@ -7,7 +7,8 @@ from .models import Car
 class CarSearchFilter(django_filters.FilterSet):
     brand = django_filters.CharFilter(lookup_expr='icontains', widget=TextInput(attrs={'placeholder': 'Volvo'}))
     model = django_filters.CharFilter(lookup_expr='icontains', widget=TextInput(attrs={'placeholder': 'V60'}))
+    location = django_filters.CharFilter(lookup_expr='icontains', widget=TextInput(attrs={'placeholder': 'Wrocław'}))
 
     class Meta:
         model = Car
-        fields = ['brand', 'model']
+        fields = ['brand', 'model','location']

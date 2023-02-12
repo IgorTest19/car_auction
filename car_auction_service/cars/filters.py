7e53814682ca296ece242aec5794faa9d304324b
IgorTest19 <quattroporte54@gmail.com>
2023-02-12 23:22:11 +0100

    adding docstrings to filters.py file

--- car_auction_service/cars/filters.py
+++ car_auction_service/cars/filters.py
@@ -5,10 +5,12 @@ from .models import Car
 
 
 class CarSearchFilter(django_filters.FilterSet):
+    """Search filter form class for cars queryset."""
     brand = django_filters.CharFilter(lookup_expr='icontains', widget=TextInput(attrs={'placeholder': 'Volvo'}))
     model = django_filters.CharFilter(lookup_expr='icontains', widget=TextInput(attrs={'placeholder': 'V60'}))
     location = django_filters.CharFilter(lookup_expr='icontains', widget=TextInput(attrs={'placeholder': 'Wrocław'}))
 
     class Meta:
+        """Metadata class."""
         model = Car
         fields = ['brand', 'model','location']

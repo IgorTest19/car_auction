192de52b46837a5d1999761451f7479885f64ce6
IgorTest19 <quattroporte54@gmail.com>
2023-02-16 21:21:44 +0100

    fixing security

--- car_auction_service/car_auction_service/settings.py
+++ car_auction_service/car_auction_service/settings.py
@@ -20,8 +20,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-_hh^qy6m5$z^$f*ejjq_fuz_@v+yupz4tww4flc8$zj9slfitn"
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True

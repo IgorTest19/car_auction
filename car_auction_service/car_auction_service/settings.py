33967d3f0c83a6c7ca07102ea21e899746c9c358
IgorTest19 <quattroporte54@gmail.com>
2023-02-19 23:34:59 +0100

    installed and added crispy forms

--- car_auction_service/car_auction_service/settings.py
+++ car_auction_service/car_auction_service/settings.py
@@ -20,6 +20,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = "django-insecure-_hh^qy6m5$z^$f*ejjq_fuz_@v+yupz4tww4flc8$zj9slfitn"
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -41,6 +43,7 @@ INSTALLED_APPS = [
     "users.apps.UsersConfig",
     "maps.apps.MapsConfig",
     "debug_toolbar",
+    "crispy_forms",
 
 ]
 

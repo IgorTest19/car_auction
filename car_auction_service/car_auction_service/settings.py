17d382d651d3d81059125c5ce0dfae12d2ae80cd
IgorTest19 <quattroporte54@gmail.com>
2023-02-21 19:58:21 +0100

    changing settigns redirection urls

--- car_auction_service/car_auction_service/settings.py
+++ car_auction_service/car_auction_service/settings.py
@@ -140,8 +140,8 @@ MEDIA_URL = "/media/"
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
-LOGIN_REDIRECT_URL = "/cars"
-LOGOUT_REDIRECT_URL = "/cars"
+LOGIN_REDIRECT_URL = "/"
+LOGOUT_REDIRECT_URL = "/"
 
 INTERNAL_IPS = [
     "127.0.0.1"

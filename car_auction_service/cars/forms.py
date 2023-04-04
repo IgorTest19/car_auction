f6bc04fd49b3c721849f16e37e52702e4e6ef23c
IgorTest19 <quattroporte54@gmail.com>
2023-04-05 00:27:49 +0200

    modifeid cars forms

--- car_auction_service/cars/forms.py
+++ car_auction_service/cars/forms.py
@@ -1,14 +1,14 @@
 from django import forms
 
-from .models import Car, CarImage
+from .models import CarAdvert, CarImage
 
 
-class CarAddForm(forms.ModelForm):
+class CarAdvertAddForm(forms.ModelForm):
     """Form class for adding a new car."""
     class Meta:
         """Metadata class."""
-        model = Car
-        fields = ['brand', 'model', 'year', 'location', 'price', 'fuel_type', 'engine_capacity']
+        model = CarAdvert
+        fields = ['brand', 'model', 'year', 'location', 'price', 'currency', 'fuel_type', 'engine_capacity']
 
 
 class ImageForm(forms.ModelForm):

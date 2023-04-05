a9da8bb5e570673c1442533b4c5dfb189f13f6ef
IgorTest19 <quattroporte54@gmail.com>
2023-04-05 23:14:13 +0200

    Adding car ad model

--- car_auction_service/cars/models.py
+++ car_auction_service/cars/models.py
@@ -95,3 +95,34 @@ class CarImage(models.Model):
             return self.image.url
         else:
             return None
+
+class CarAd(models.Model):
+    """
+    Stores a single car advertisement. Related to:
+    :model: 'cars.car
+    """
+    CURRENCY_CHOICES = (
+        ('pln', 'PLN'),
+        ('eur', 'EURO')
+    )
+    PRICE_TYPE_CHOICES = (
+        ('gross', 'GROSS'),
+        ('net', 'NET')
+    )
+    owner = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='car_ad_owner')  # modify relation to onetoone field
+    car = models.ForeignKey(Car, on_delete=models.CASCADE, related_name='car_ad')
+    description = models.TextField(blank=True, null=True)
+    price = models.DecimalField(max_digits=10, decimal_places=2, blank=True, null=True)
+    currency = models.CharField(max_length=10, choices=CURRENCY_CHOICES, default='pln')
+    price_type = models.CharField(max_length=10, choices=PRICE_TYPE_CHOICES, defailt='gross')
+    published = models.DateTimeField(default=timezone.now)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+    valid_unitl = models.DateTimeField()
+    is_valid = models.BooleanField(default=True)
+
+    class Meta:
+        ordering = ['-published']
+
+    def __str__(self):
+        return f'{self.car.brand} {self.cvar.model} ({self.price} {self.currency} {self.price_type}'

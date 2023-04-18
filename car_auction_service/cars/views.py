0bc6c2b1241bcff55a9a757829ac70c003b68c30
IgorTest19 <quattroporte54@gmail.com>
2023-04-18 23:59:52 +0200

    cleaning up views.py

--- car_auction_service/cars/views.py
+++ car_auction_service/cars/views.py
@@ -53,8 +53,6 @@ def car_advert_detail(request, pk):
     """
     car_advert = get_object_or_404(CarAdvert, pk=pk)
     car_images = reversed(get_list_or_404(CarImage, car_advert=car_advert))
-    print('------------car_images')
-    print(car_images)
 
     # Adding map component
     # Getting location from car model
@@ -214,7 +212,6 @@ def dashboard(request):
             # Form with no data after adding a car
             return HttpResponseRedirect(request.META.get('HTTP_REFERER'))
         else:
-            print(car_advert_add_form.errors)
             messages.add_message(request, messages.INFO, "Failed to add a car")
     else:
         car_advert_add_form = CarAdvertAddForm()

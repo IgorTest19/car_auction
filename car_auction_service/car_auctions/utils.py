0062f599972a6ed9dba780362e1a291e8b78c4d2
IgorTest19 <quattroporte54@gmail.com>
2023-05-15 22:54:38 +0200

    refactoring code for similar cars

--- car_auction_service/car_auctions/utils.py
+++ car_auction_service/car_auctions/utils.py
@@ -1,43 +1,114 @@
 """car_auctions helper functionalities"""
 from decimal import Decimal
 
-from django.db.models import Max, Q
-from django.shortcuts import get_list_or_404, get_object_or_404, redirect, render
-from .models import CarAdvert, RecentlyViewed
+from django.db.models import Q
+from .models import CarAdvert
+
+
+# def get_similar_cars(car_advert):
+#     price_range = 100
+#     car_ads_by_price = []
+#
+#     print(f'------------ car_advert price: {car_advert.price}')
+#     for percent in range(1, price_range + 1):
+#         print(f'--------- percent: {percent}, length of the car_ads_by_price: {len(car_ads_by_price)})')
+#         # 80000 * (1 - 1) * 0,001
+#         print(
+#                 f'------------ car_advert_prices range: {car_advert.price * (1 - percent * Decimal(0.01))} - {car_advert.price * (1 + percent * Decimal(0.01))}')
+#         print(
+#                 f'------------ car prices percentage range: {car_advert.price * (1 - percent * Decimal(0.01))} - {car_advert.price * (1 + percent * Decimal(0.01))}')
+#         car_ads_by_price += CarAdvert.objects.filter(Q(brand=car_advert.brand) | Q(price__gte=car_advert.price * (1 - percent * Decimal(0.01)), price__lte=car_advert.price * (1 + percent * Decimal(0.01))))[:5]
+#         if len(car_ads_by_price) == 5:
+#             return car_ads_by_price
+#
+#     print(f'---------------- car_ads_by_price')
+#     print(car_ads_by_price)
+#     same_price_car_ads = CarAdvert.objects.filter(Q(brand=car_advert.brand) | Q(price=car_advert.price))[:5]
+#     if same_price_car_ads:
+#         return same_price_car_ads
+#     return car_ads_by_price
 
 
 
-def get_similar_cars(car_advert, recently_viewed):
-    # print(f'---- get_similar_cars is working from .utils level')
-    # print(f'---- passed car')
-    # print(recently_viewed)
-    #
-    # if CarAdvert.objects.filter(price=car_advert.price):
-    #     price = CarAdvert.objects.filter(price=car_advert.price)
-    # elif CarAdvert.objects.filter(price__gte=car_advert.price*0.80, price__lte=car_advert.price*1.20)
-    #     price = CarAdvert.objects.filter(price__gte=car_advert.price*0.08, price__lte=car_advert.price*1.20)
-    # elif CarAdvert.objects.filter(price__gte=car_advert.price*0.80, price__lte=car_advert.price*1.40)
-    #
-    #
-    # # Suggested similar car advertisements
-    # similar_car_ads = CarAdvert.objects.filter((Q(brand=car_advert.brand) | Q(model=car_advert.model)) & Q(
-    #     price__range=[car_advert.price * Decimal(0.8), car_advert.price * Decimal(1.2)]))[:5]
-    # print('-------------------similar car ads')
-    # print(similar_car_ads)
-    #
-    # tested_return = f'returned get_similar_cars car {car}'
-    # return tested_return
-    pass
+# def get_similar_cars(car_advert):
+#     price_range = 100
+#     car_ads_by_price = []
+#
+#     print(f'------------ car_advert price: {car_advert.price}, car_advert.brand: {car_advert.brand}')
+#     for percent in range(1, price_range + 1):
+#         print(f'--------- percent: {percent}, length of the car_ads_by_price: {len(car_ads_by_price)})')
+#         # 80000 * (1 - 1) * 0,001
+#         print(
+#                 f'------------ car_advert_prices range: {car_advert.price * (1 - percent * Decimal(0.01))} - {car_advert.price * (1 + percent * Decimal(0.01))}')
+#         print(
+#                 f'------------ car prices percentage range: {car_advert.price * (1 - percent * Decimal(0.01))} - {car_advert.price * (1 + percent * Decimal(0.01))}')
+#         car_ads_by_price += CarAdvert.objects.filter(~Q(id=car_advert.id) & Q(brand=car_advert.brand) & Q(price__gte=car_advert.price * (1 - percent * Decimal(0.01)), price__lte=car_advert.price * (1 + percent * Decimal(0.01))))[:5]
+#
+#         if len(car_ads_by_price) == 5:
+#             # return car_ads_by_price
+#             break
+#     print(f'------------ car_ads_by_price: {car_ads_by_price}')
+#     for car in car_ads_by_price:
+#         print(car.price)
+#
+#     same_price_car_ads = CarAdvert.objects.filter(Q(brand=car_advert.brand) | Q(price=car_advert.price))[:5]
+#     if same_price_car_ads:
+#         return same_price_car_ads
+#     return car_ads_by_price
 
-def filter_by_price(car_advert):
-    max_price = CarAdvert.objects.aggregate(max_price=Max('price'))['max_price']
-    print(f'---------- max price: {max_price}')
+
+def get_similar_cars(car_advert):
     price_range = 100
-
+    similar_car_ads = []
     for percent in range(1, price_range + 1):
-        car_ads_by_price = CarAdvert.objects.filter(Q(brand=car_advert.brand) | Q(price__gte=car_advert.price*(1-percent)*Decimal(0.01), price__lte=car_advert.price*(1+percent)*Decimal(0.01)))[:5]
+        # get minimal price range for given percentage
+        price_min = car_advert.price * (1 - percent * Decimal(0.01))
 
-    same_price_car_ads = CarAdvert.objects.filter(Q(brand=car_advert.brand) | Q(price=car_advert.price))[:5]
-    if same_price_car_ads:
-        return same_price_car_ads
-    return car_ads_by_price
+        # get maximal price range for a given percentage
+        price_max = car_advert.price * (1 + percent * Decimal(0.01))
+
+        # get 5 car adverts objects filtered by a price range of price_min and price max, by a car brand and by excluding default car.
+        filtered_car_adverts = CarAdvert.objects.filter(Q(brand=car_advert.brand) & Q(price__gte=price_min, price__lte=price_max))[:5]
+
+        # exclude filtered car adverts if they are already added to the list
+
+        filtered_car_adverts_clean = filtered_car_adverts.exclude(id__in=[car.id for car in similar_car_ads])
+
+        # filtered_car_adverts_clean = filtered_car_adverts_clean.exclude(id=car_advert.id)
+
+        similar_car_ads += list(filtered_car_adverts_clean)
+
+        if len(similar_car_ads) == 5:
+            break
+            # return similar_car_ads
+
+        print(f'------------ similar_car_ads: {similar_car_ads}')
+        for car in similar_car_ads:
+            print(car.price)
+
+        return similar_car_ads
+
+
+# from django.db.models import Q
+# from .models import Car
+#
+# brand = 'Toyota'
+# base_price = 10000
+# percentage_increase = 0.01
+# num_iterations = 100
+#
+# query = Q(brand=brand)
+# cars = []
+#
+# added_car_ids = set()
+#
+# for i in range(num_iterations):
+#     min_price = base_price * (1 + i * percentage_increase)
+#     max_price = base_price * (1 + (i + 1) * percentage_increase)
+#     query &= Q(price__range=(min_price, max_price))
+#     filtered_cars = Car.objects.filter(query)[:5]
+#
+#     for car in filtered_cars:
+#         if car.id not in added_car_ids:
+#             cars.append(car)
+#             added_car_ids.add(car.id)

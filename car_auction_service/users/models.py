172765d259aa14ec24af484cff2d605d9d7c93a3
IgorTest19 <quattroporte54@gmail.com>
2023-05-31 16:15:22 +0200

    restyling code in users app

--- car_auction_service/users/models.py
+++ car_auction_service/users/models.py
@@ -6,13 +6,13 @@ from car_auctions.models import CarAdvert
 
 class UserProfile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
-    cars_observed = models.ManyToManyField(CarAdvert, related_name='cars_observed', blank=True)
+    cars_observed = models.ManyToManyField(CarAdvert, related_name="cars_observed", blank=True)
 
     def __str__(self):
         return str(self.user.username)
 
     def cars_observed_by_user(self):
-        return ','.join([str(user) for user in self.cars_observed.all()])
+        return ",".join([str(user) for user in self.cars_observed.all()])
 
     def cars_observed_by_user2(self):
         list1 = [user for user in self.cars_observed.all()]

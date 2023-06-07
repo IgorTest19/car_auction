94684521766982915bd9d2950e93eada13095467
IgorTest19 <quattroporte54@gmail.com>
2023-06-07 18:06:06 +0200

    fix

--- car_auction_service/car_auctions/views.py
+++ car_auction_service/car_auctions/views.py
@@ -221,6 +221,13 @@ def cars_observed(request):
     #  car_adverts_locations = [car_advert.location for car_advert in user_profile.cars_observed_by_user2()]
     car_adverts_observed = user_profile.cars_observed_by_user2()
 
+    x_forwarded_for = request.META.get('HTTP_X_FORWARDED_FOR')
+    print(f'----x_forwarded_for: {x_forwarded_for}')
+    if x_forwarded_for:
+        ip = x_forwarded_for.split(',')[0]
+    else:
+        ip = request.META.get('REMOTE_ADDR')
+    print(f'------ iP IS: {ip}')
     # Getting user location by IP Address
     # user_ip = request.META.get('REMOTE_ADDR')
     # latitude, longitude = get_user_location((user_ip))
@@ -238,7 +245,6 @@ def cars_observed(request):
 
     for car_advert in car_adverts_observed:
         # Getting location values
-        location_values = geocoder.osm(f'{car_advert.location}, Poland')
         location_values = geocoder.osm(f'{car_advert.location}, Poland')
         # Adding a map marker
         folium.Marker(location_values.latlng, tooltip=car_advert.location, popup=f'<img src="{ car_advert.get_first_image() }" width="50px" height="50px" alt="img">').add_to(map)
